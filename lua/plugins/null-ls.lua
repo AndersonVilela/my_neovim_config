48f1a144d4c9770741b5c33fdaf4a43a499136a8
AndersonVilela <andersonvilela.al@gmail.com>
2023-06-26 23:10:09 -0300

    add prettier js

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,31 +1,42 @@
+
 local null_ls = require("null-ls")
 local helpers = require("null-ls.helpers")
 
+-- Configuração do Prettier
+local prettier = {
+    method = null_ls.methods.FORMATTING,
+    filetypes = { "vue", "javascript", "typescript", "javascript.jsx", "typescript.tsx", "html", "css" },
+    generator = null_ls.generator({
+        command = "prettier",
+        -- Você pode adicionar outras opções do Prettier aqui, se necessário
+        args = { "--stdin-filepath", "$FILENAME" },
+        to_stdin = true,
+        from_stdin = true,
+        format = "raw",
+    }),
+}
+
+null_ls.register(prettier)
+
+-- Configuração do Markdownlint
 local markdownlint = {
     method = null_ls.methods.DIAGNOSTICS,
     filetypes = { "markdown" },
-    -- null_ls.generator creates an async source
-    -- that spawns the command with the given arguments and options
     generator = null_ls.generator({
         command = "markdownlint",
         args = { "--stdin" },
         to_stdin = true,
         from_stderr = true,
-        -- choose an output format (raw, json, or line)
         format = "line",
         check_exit_code = function(code, stderr)
             local success = code <= 1
 
             if not success then
-                -- can be noisy for things that run often (e.g. diagnostics), but can
-                -- be useful for things that run on demand (e.g. formatting)
                 print(stderr)
             end
 
             return success
         end,
-        -- use helpers to parse the output from string matchers,
-        -- or parse it manually with a function
         on_output = helpers.diagnostics.from_patterns({
             {
                 pattern = [[:(%d+):(%d+) [%w-/]+ (.*)]],
@@ -40,3 +51,4 @@ local markdownlint = {
 }
 
 null_ls.register(markdownlint)
+
